84719517d98fd6c823125115a78d44d894332cd2
Scott Dockendorf <scott@dockendorf.org>
2017-03-08 18:14:11 -0600

    Added base set of chocolatey.org commands for my .net dev environment

---
+++ DevEnvSetup.bat
@@ -0,0 +1,37 @@
+rem ***** Scott's Dev Environment Chocolatey Script
+
+rem ----- OS integration 
+choco install 7zip
+
+rem ----- Browsers
+choco install googlechrome 
+choco install firefox
+
+rem ----- Text editors
+choco install notepadplusplus
+choco install sublimetext3
+
+rem ----- Source Control: git
+choco install git 
+choco install poshgit
+
+rem ----- Misc Dev Tools 
+choco install filezilla
+choco install sysinternals 
+choco install fiddler
+choco install fiddler4
+choco install poshtools-visualstudio2015
+choco install ilspy
+choco install sizer
+
+rem ----- DevOps Tools 
+choco install mremoteng
+
+rem ----- Non-development tools 
+choco install adobereader
+choco install skype
+choco install paint.net
+choco install evernote
+choco install skitch
+choco install slack
+choco install wunderlist
