d0625a4dfbb32144164dc2e1deff133fbecfafbb
Scott Dockendorf <scott@dockendorf.org>
2017-03-10 09:43:21 -0600

    Added AzureStorageEmulator & Paperclip batch files

---
+++ StartAzureStorageEmulator.bat
@@ -0,0 +1,5 @@
+cd "C:\Program Files (x86)\Microsoft SDKs\Azure\Storage Emulator"
+AzureStorageEmulator.exe stop
+AzureStorageEmulator.exe start
+rem AzureStorageEmulator.exe clear 
+AzureStorageEmulator.exe status
---
+++ StartPaperclip.bat
@@ -0,0 +1,2 @@
+C:\utils\papercut\papercut.service.exe
+C:\utils\papercut\papercut.exe
